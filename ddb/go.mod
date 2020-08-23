69578ff8cf260a7f35bce378e2b934e8296b177f
Henry Syahputra <askzenri@gmail.com>
2020-08-23 19:26:26 +0800

    sdasdsad

---
+++ ddb/go.mod
@@ -0,0 +1,5 @@
+module github.com/hsputra/ot8/ddb
+
+go 1.15
+
+require github.com/jinzhu/gorm v1.9.16
---
+++ main.go
@@ -0,0 +1,12 @@
+package main
+
+import (
+	"fmt"
+	"github.com/hsputra/ot8/ddb"
+
+)
+
+func main()  {
+	fmt.Println("hello")
+	//ddb.DB
+}
